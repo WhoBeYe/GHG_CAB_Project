e9926054ce893c3491af263d3decb3a2ef08e8c7
RacerElijah77 <gasatae1@tcnj.edu>
2023-04-06 21:48:10 -0400

    added code to zipcode csv migration

--- scripts/create_zipcode_data.py
+++ scripts/create_zipcode_data.py
@@ -2,24 +2,27 @@
 import psycopg2
 
 # Link the database called demo_database1
-conn = psycopg2.connect("host=localhost dbname=demo_database1 user=postgres")
+conn = psycopg2.connect("host=localhost dbname=CAB_database user=postgres")
 cur = conn.cursor()
 
-# This command can only be executed once after the table has been created
+# This command can only be executed once after the table has been created, comment out
 # cur.execute("""
-#     CREATE TABLE users(
-#         id integer PRIMARY KEY,
-#         email text,
-#         name text,
-#         address text
+#     CREATE TABLE zip_code_table(
+#         zip integer PRIMARY KEY,
+#         zip_name varchar(50),
+#         city varchar(50),
+#         state char(2),
+#         county_name varchar(50) 
 #     )
 # """)
 
-# Testing 
+with open('/home/lion/github-classroom/TCNJ-degoodj/cab-project-6/data_files/zipcode_data.csv', 'r') as f:
+    
+    next(f)
+    cur.copy_from(f, 'zip_code_table', sep=',')
 
-cur.execute("INSERT INTO users VALUES (%s, %s, %s, %s)", (10, 'hello@dataquest.io', 'Some Name', '123 fake st.'))
 conn.commit()
 
-cur.execute('SELECT * FROM users')
+cur.execute('SELECT * FROM zip_code_table')
 
 all = cur.fetchall()
