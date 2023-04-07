051f32540ef0fc35728e377c44c7e8679eccb1d3
RacerElijah77 <gasatae1@tcnj.edu>
2023-04-06 20:29:41 -0400

    Added tutorial file for psycopg2

---
+++ demo_loading.py
@@ -0,0 +1,19 @@
+import psycopg2
+conn = psycopg2.connect("host=localhost dbname=demo_database1 user=postgres")
+cur = conn.cursor()
+
+# cur.execute("""
+#     CREATE TABLE users(
+#         id integer PRIMARY KEY,
+#         email text,
+#         name text,
+#         address text
+#     )
+# """)
+
+cur.execute("INSERT INTO users VALUES (%s, %s, %s, %s)", (10, 'hello@dataquest.io', 'Some Name', '123 fake st.'))
+conn.commit()
+
+cur.execute('SELECT * FROM users')
+
+all = cur.fetchall()
