5ce14e42578ecf592596901117c6137a98ba5ddd
RacerElijah77 <gasatae1@tcnj.edu>
2023-04-06 21:57:47 -0400

    Added changes to the zip_code data database

--- scripts/create_zipcode_data.py
+++ scripts/create_zipcode_data.py
@@ -6,17 +6,16 @@ conn = psycopg2.connect("host=localhost dbname=CAB_database user=postgres")
 cur = conn.cursor()
 
 # This command can only be executed once after the table has been created, comment out
-# cur.execute("""
-#     CREATE TABLE zip_code_table(
-#         zip integer PRIMARY KEY,
-#         zip_name varchar(50),
-#         city varchar(50),
-#         state char(2),
-#         county_name varchar(50) 
-#     )
-# """)
+cur.execute("""
+    CREATE TABLE zip_code_table(
+        zip integer PRIMARY KEY,
+        city varchar(50),
+        state char(2),
+        county_name varchar(50) 
+    )
+""")
 
-with open('/home/lion/github-classroom/TCNJ-degoodj/cab-project-6/data_files/zipcode_data.csv', 'r') as f:
+with open('/home/lion/github-classroom/TCNJ-degoodj/cab-project-6/data_files/zip_codes.csv', 'r') as f:
     
     next(f)
     cur.copy_from(f, 'zip_code_table', sep=',')
